ba75c47f8216694804045661ab2715abe00a17cf
Fabio Percivaldi <fabio.percivaldi@mia-platform.eu>
2020-02-05 21:21:47 +0100

    trying the redirect url

--- src/App.js
+++ src/App.js
@@ -18,10 +18,34 @@ class App extends Component {
       isAuthenticating: true
     };
   }
-
+  getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    console.log('$$$$$$$$$$$$$$$$', userAgent)
+        // Windows Phone must come first because its UA also contains "Android"
+      if (/windows phone/i.test(userAgent)) {
+          return "Windows Phone";
+      }
+  
+      if (/android/i.test(userAgent)) {
+          return "Android";
+      }
+  
+      // iOS detection from: http://stackoverflow.com/a/9039885/177710
+      if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+          return "iOS";
+      }
+  
+      return "unknown";
+  }
   async componentDidMount() {
     this.loadFacebookSDK();
-
+    const clientType = this.getMobileOperatingSystem()
+    if (clientType === 'iOS') {
+      window.location.href = 'https://apps.apple.com/it/app/feriapp/id1488392565'
+    }
+    if (clientType === 'Android') {
+      window.location.href = 'https://play.google.com/store/apps/details?id=it.feriapp&gl=IT'
+    }
     try {
       await Auth.currentAuthenticatedUser();
       this.userHasAuthenticated(true);
