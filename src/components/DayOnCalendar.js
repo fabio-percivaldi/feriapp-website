f42801e0a24b0d350f46a92470c35af43bd704e8
Fabio Percivaldi <fabio.percivaldi@mia-platform.eu>
2020-02-08 21:48:33 +0100

    past days are marked with stripes

--- src/components/DayOnCalendar.js
+++ src/components/DayOnCalendar.js
@@ -3,6 +3,7 @@ import { Row, Col, Tooltip, OverlayTrigger, Button } from "react-bootstrap";
 import './DayOnCalendar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLock, faLockOpen } from '@fortawesome/free-solid-svg-icons'
+import moment from 'moment'
 export default class DayOnCalendar extends Component {
     constructor(props) {
         super(props)
@@ -28,15 +29,21 @@ export default class DayOnCalendar extends Component {
     }
     renderLockButton = () => {
         // TODO: render the button only if is not a weekend day or holiday
-        if ((!this.props.isHoliday && !this.props.isWeekend) || this.state.isLocked) {
-            return <Button className={this.state.isLocked ? 'unlock-btn' : 'lock-btn'} style={{ marginTop: 'auto', marginBottom: 'auto', marginLeft:'auto' }} onClick={this.toggleLock}>
+        if (((!this.props.isHoliday && !this.props.isWeekend) || this.state.isLocked) && !this.isDayInThePast(this.props.day)) {
+            return <Button className={this.state.isLocked ? 'unlock-btn' : 'lock-btn'} style={{ marginTop: 'auto', marginBottom: 'auto', marginLeft: 'auto' }} onClick={this.toggleLock}>
                 <FontAwesomeIcon icon={this.state.isLocked ? faLock : faLockOpen}></FontAwesomeIcon>
             </Button>
         }
     }
+    isDayInThePast = (day) => {
+        return moment(day).isBefore(moment().subtract(1, 'days'))
+    }
     render() {
         let className = this.props.isWeekend ? 'weekend' : 'defaultDay'
         className = this.props.isHoliday ? 'holiday' : className
+        if (this.isDayInThePast(this.props.day)) {
+            className += ' past-day'
+        }
         return (
             <Col className={className}>
                 <Row style={{ textAlign: 'center', justifyContent: 'flex-start' }}>
