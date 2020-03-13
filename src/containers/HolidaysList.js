ec08e5f854683a642d4a2da701c69289959c67f4
Fabio Percivaldi <fabio.percivaldi@mia-platform.eu>
2020-03-13 01:05:21 +0100

    fix on ig media card

--- src/containers/HolidaysList.js
+++ src/containers/HolidaysList.js
@@ -4,14 +4,14 @@ import { Card, Container, Col } from 'react-bootstrap'
 import { connect } from "react-redux";
 
 const renderHoliday = (holiday, index) => {
-    const { caption, mediaUrl, permalink } = holiday
-    return <a key={index} style={{cursor: 'pointer', width: '15%', height: '300px', marginBottom: 'auto', marginTop: 'auto'}} href={permalink} rel="noopener noreferrer" target="_blank">
+    const { caption, mediaUrl, permaurl } = holiday
+    return <a key={index} style={{cursor: 'pointer', width: '15%', height: '300px', marginBottom: 'auto', marginTop: 'auto'}} href={permaurl} rel="noopener noreferrer" target="_blank">
         <Card className='grow' style={{ height: '100%', boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 3px 10px 0 rgba(0, 0, 0, 0.19)', borderRadius: '7px' }}>
             <Card.Img style={{ height: '60%', borderTopRightRadius: '7px', borderTopLeftRadius: '7px' }} variant="top" src={`${mediaUrl}`} />
             <Card.Body style={{ display: 'flex' }}>
                 <Card.Title style={{ textAlign: 'center', color: '#f87825', margin: 'auto' }}>{}</Card.Title>
                 <Card.Text style={{ textAlign: 'center', color: '#0b2d50', margin: 'auto', fontWeight: 'bold' }}>
-                    {caption.split('#')[0]}
+                    {caption.split('?')[0]}
                 </Card.Text>
             </Card.Body>
         </Card>
